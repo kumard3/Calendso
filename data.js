218700589b06648fceaf2b46df1cc0cded372341
Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>
2021-06-04 15:00:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,75 @@
+
+const   products  = [
+   [{
+        _id:'123',
+        name: 'Slim Shirt',
+        image: '/images/a.jpg',
+        category: 'Shirts',
+        price: 60,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 10
+
+    },
+    { _id:'1',
+        name: 'Slim Shirt',
+        image: '/images/a.jpg',
+        category: 'Shirts',
+        price: 40,
+        brand: 'Nike',
+        rating: 3.5,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: 'Slim Shirt',
+        image: '/images/a.jpg',
+        price: 60,
+        category: 'Shirts',
+        brand: 'Nike',
+        rating: 4,
+        numReviews: 10
+    },
+    { _id:'3',
+        name: 'Slim Shirt',
+        image: '/images/a.jpg',
+        price: 90,
+        brand: 'Nike',
+        rating: 4.5,
+        category: 'Shirts',
+        numReviews: 10
+    },
+    {
+        _id:'4',
+        name: 'Slim Shirt',
+        image: '/images/a.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        category: 'Pants',
+        numReviews: 10
+    },
+    {
+        _id:'5',
+        name: 'Slim Shirt',
+        image: '/images/a.jpg',
+        price: 80,
+        brand: 'Nike',
+        rating: 4.5,
+        category: 'Pants',
+        numReviews: 10
+    },
+    {
+        _id:'6',
+        name: 'Slim Shirt',
+        image: '/images/a.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.5,
+        category: 'Pants',
+        numReviews: 10
+    },
+
+    ]
+]
+export default products
---
+++ pages/api/book/[user].ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+import { createEvent, CalendarEvent } from '../../../lib/calendarClient';
+import createConfirmBookedEmail from "../../../lib/emails/confirm-booked";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { user } = req.query;
+
+    const currentUser = await prisma.user.findFirst({
+        where: {
+          username: user,
+        },
+        select: {
+            credentials: true,
+            timeZone: true,
+            email: true,
+            name: true,
+        }
+    });
+
+    const evt: CalendarEvent = {
+        type: req.body.eventName,
+        title: req.body.eventName + ' with ' + req.body.name,
+        description: req.body.notes,
+        startTime: req.body.start,
+        endTime: req.body.end,
+        location: req.body.location,
+        organizer: { email: currentUser.email, name: currentUser.name, timeZone: currentUser.timeZone },
+        attendees: [
+            { email: req.body.email, name: req.body.name, timeZone: req.body.timeZone }
+        ]
+    };
+
+    const result = await createEvent(currentUser.credentials[0], evt);
+
+    if (!result.disableConfirmationEmail) {
+        createConfirmBookedEmail(
+          evt
+        );
+    }
+
+    res.status(200).json(result);
+}
